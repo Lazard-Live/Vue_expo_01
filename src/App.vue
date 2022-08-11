f80d42f31c295b438f6478e1003a858d990d6882
Lazard-Live <workbd@bk.ru>
2022-08-11 12:15:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="src/assets/logo.svg"
+      width="50"
+      height="50"
+    />
+
+    <div>
+      <input type="text" />
+      <button>Добавить</button>
+      <div class="list"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
